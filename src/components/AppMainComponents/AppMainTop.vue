e20064091e410fbe574485252d1056d01f7c6afa
NicholasBuiatti <nic.buiatti@gmail.com>
2024-05-30 17:30:29 +0200

    visualizzazione dettagli all'hover dei poster

--- src/components/AppMainComponents/AppMainTop.vue
+++ src/components/AppMainComponents/AppMainTop.vue
@@ -24,31 +24,37 @@ export default {
 
     },
     mounted() {
-        for (let i = 0; i < array.length; i++) {
-            const element = array[i];
 
-        }
     }
 }
 </script>
 
 <template>
     <h4 class="text-white">Sono stati trovati {{ this.store.moviesList.length }} film</h4>
-    <div class="row">
-        <div v-for="movie in this.store.moviesList" class="col-3 bg-warning m-2">
-            <!-- <pre>{{ movie }}</pre> -->
-            <img :src="`https://image.tmdb.org/t/p/w342/${movie.backdrop_path}`" alt="">
-            <h5>Titolo: {{ movie.title }}</h5>
-            <h5>Titolo Originale: {{ movie.original_title }}</h5>
-            <h5>Lingua: {{ movie.original_language }}</h5>
-            <img :src="getImg(movie.flag)" :class="movie.flag == 'No flag' ? 'd-none' : ''" alt="" id="flag">
-            // APPROSIMAZIONE DEL VOTO AL LIMITE PIù VICINO DIVIDENDOLO PER 2
-            <h5>Voto: {{ Math.round(movie.vote_average / 2) }}</h5>
-            // METTO TANTE STELLE QUANTO è IL VOTO
-            <span v-for="star in Math.round(movie.vote_average / 2)">★</span>
-            <span v-for="noStar in (5 - (Math.round(movie.vote_average / 2)))">☆</span>
+
+    <!-- CREO UN CONTAINER BOX CON OVERFLOW PER CONTENERE I MIEI POSTER SU UN UNICA RIGA -->
+    <div class="row flex-nowrap overflow-hidden w-100">
+        <div v-for="movie in this.store.moviesList" class="col-3 position-relative" id="containerPoster">
+            <img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" class="w-100" alt="" id="poster">
+
+            <div class="text-white p-2 position-absolute" id="infoMovie">
+                <h6>Titolo originale: {{ movie.original_title }}</h6>
+                <h6>Titolo tradotto: {{ movie.title }}</h6>
+                <h6>Lingua originale: {{ movie.original_language }}</h6>
+                <img :src="getImg(movie.flag)" :class="movie.flag == 'No flag' ? 'd-none' : ''" alt="" id="flag">
+                <!-- APPROSIMAZIONE DEL VOTO AL LIMITE PIù VICINO DIVIDENDOLO PER 2 -->
+                <h6>Voto: {{ Math.round(movie.vote_average / 2) }}</h6>
+                <!-- METTO TANTE STELLE QUANTO è IL VOTO E NON MI PIACE CHE UN 3.4 DIVENTI UN 4 -->
+                <span v-for="star in Math.round(movie.vote_average / 2)">★</span>
+                <span v-for="noStar in (5 - (Math.round(movie.vote_average / 2)))">☆</span>
+                <h6>{{ movie.release_date }}</h6>
+            </div>
+
+
         </div>
+
     </div>
+
 
     <pre class="bg-primary">{{ this.store.moviesList }}</pre>
 </template>
@@ -57,5 +63,20 @@ export default {
 #flag {
     width: 5rem;
     height: 3rem;
+}
+
+#poster {
+    height: 20rem;
+}
+
+#infoMovie {
+    background-color: #222;
+    height: 20rem;
+    width: 14rem;
+    border: 1px solid white;
+}
+
+#containerPoster:hover>#poster {
+    display: none;
 }
 </style>
